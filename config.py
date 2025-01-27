2480bba44e01cc8cef7f7bdaf615476ffb2b4e80
projectskl17 <hermit0bot@gmail.com>
2025-01-27 22:11:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ OWNER_ID = int(os.getenv("OWNER_ID", 0))
 
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split()))
 
-CHANNELS = os.getenv("CHANNELS", "@testkl17,@terab0xdl").split(',')
+CHANNELS = list(map(int, os.getenv("CHANNELS", "-1002245937378, -1001711146727").split(',')))
